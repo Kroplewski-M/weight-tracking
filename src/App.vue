71d1f87fb1d29d27e50a268135057119633815ae
Mateusz <kroplewskimateusz@gmail.com>
2022-09-05 21:36:55 +0100

    added weight history place holder

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,21 @@
         </div>
     </section>
 
-    <section>
-      <p class="font-bold h-[100vh] text-zinc-300 mt-[100px]">Weight history-</p>
-        <div>
-          
+    <section class="h-[100vh]">
+      <div class="flex text-zinc-300 mt-[100px]">
+        <p class="font-bold ">Weight history-</p>
+        <p class="ml-[180px]">View:</p>
+        <a href="#" class="ml-[20px] font-bold underline">Tabs</a>
+        <a href="#" class="ml-[30px]">Graph</a>
+      </div>
+        <div class="w-[500px] h-[100px] bg-[#222222] rounded-md mt-10 mx-auto text-zinc-300 text-center hover:scale-[1.1] hover:cursor-pointer transition-transform	">
+            <p class="font-semibold text-[30px] pt-[25px]">Feb 17th 2020 - 80kg</p>
+        </div>
+        <div class="w-[500px] h-[100px] bg-[#222222] rounded-md mt-10 mx-auto text-zinc-300 text-center hover:scale-[1.1] hover:cursor-pointer transition-transform	">
+            <p class="font-semibold text-[30px] pt-[25px]">Jan 10th 2020 - 90kg</p>
+        </div>
+        <div class="w-[500px] h-[100px] bg-[#222222] rounded-md mt-10 mx-auto text-zinc-300 text-center hover:scale-[1.1] hover:cursor-pointer transition-transform	">
+            <p class="font-semibold text-[30px] pt-[25px]">Dec 20th 2019 - 950kg</p>
         </div>
     </section>
   </div>
