10c72d189211438be9d392c5b04ffffba37a6c07
Mateusz <kroplewskimateusz@gmail.com>
2022-09-05 20:05:14 +0100

    started rough layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-[800px] mx-auto">
+    <section>
+        <p class="text-zinc-300 text-center pt-5 pb-[10px] text-[20px]">Welcome, Mateusz</p>
+        <div class="w-[400px] h-[200px] rounded-tl-[510px] rounded-tr-[510px] bg-transparent border-solid border-[15px] border-purple-500 border-b-0 mt-[10px] mx-auto text-center">
+          <p class="font-bold text-zinc-300 mt-10">Current Weight-</p>
+          <p class="font-bold text-[70px] text-zinc-300 mt-[30px]">85kg</p>
+        </div>
+    </section>
+
+    <section>
+      <p class="font-bold h-[100vh] text-zinc-300 mt-[100px]">Weight history-</p>
+        <div>
+          
+        </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
